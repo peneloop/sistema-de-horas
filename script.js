cb1084377fb6213499706510387902a2785f7411
Penélope Vieira <vieirapenelope21@gmail.com>
2026-02-19 14:43:41 -0300

    Update time variable to use current hour

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let hora = 23
+let agora = new Date()
+let hora = agora.getHours()
 console.log(`são exatamente ${hora} horas.`)
 
 if ( hora >=0 && hora <= 5 ){
     console.log("é madrugada.")
     console.log("durma bem.")
-} else if (hora >=6 && hora <= 11){
+} else if (hora >=6 && hora <= 11){ 
     console.log("é manhã.")
     console.log("bom dia")
 }else if (hora >=12 && hora <=17){
