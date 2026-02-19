0463497123a444f6e5593cf666d261715afe9c54
Penélope Vieira <vieirapenelope21@gmail.com>
2026-02-19 19:03:44 -0300

    Refactor time and day display logic into carregar function

--- script.js
+++ script.js
@@ -1,58 +1,21 @@
-//horas
-
-let agora = new Date()
-let hora = agora.getHours()
-console.log(`são exatamente ${hora} horas.`)
-
-if ( hora >=0 && hora <= 5 ){
-    console.log("é madrugada.")
-    console.log("durma bem.")
-} else if (hora >=6 && hora <= 11){ 
-    console.log("é manhã.")
-    console.log("bom dia")
-}else if (hora >=12 && hora <=17){
-    console.log("é tarde.")
-    console.log("boa tarde")
-}else{
-    console.log("é noite.")
-    console.log("boa noite, hora de dormir.")
+function carregar(){
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são <strong>${hora} horas.</strong>`
+    if (hora >= 0 && hora <12){
+        img.src= 'fotos/foto-manha.jpg'
+        document.body.style.background = "#e2cd9f"
+        document.querySelector('section').style.boxShadow = "5px 5px 10px rgba(203, 172, 105, 0.59)"
+        document.box-shadow
+    } else if (hora >=12 && hora < 18){
+        img.src = 'fotos/foto-tarde.jpg'
+        document.body.style.background = "#de8d29"
+        document.querySelector('section').style.boxShadow = "5px 5px 10px rgba(117, 66, 4, 0.76)"
+    } else {
+        img.src='fotos/foto-noite.jpg'
+        document.body.style.background = "#14033a"
+        document.querySelector('section').style.boxShadow = "5px 5px 10px rgba(14, 3, 37, 0.73)" 
+    }
 }
-
-//dia da semana
-let agora = new Date()
-let diaSem = agora.getDay()
-/* 
-    0 = domingo
-    1 = segunda
-    2 = terça
-    3 = quarta
-    4 = quinta
-    5 = sexta
-    6 = sabado
-*/
-
-switch(diaSem){
-    case 0:
-        console.log("domingo")
-        break
-    case 1:
-        console.log("segunda")
-        break
-    case 2:
-        console.log("terça")
-        break
-    case 3:
-        console.log("quarta")
-        break
-    case 4:
-        console.log("quinta")
-        break
-    case 5:
-        console.log("sexta")
-        break
-    case 6:
-        console.log("sabado")
-        break
-    default:
-        console.log("vc ta no nether")
-} 
