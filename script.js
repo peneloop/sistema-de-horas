0592842cb296e840e0f1e2cb81d2a031fb9a298d
Penélope Vieira <vieirapenelope21@gmail.com>
2026-02-19 14:39:17 -0300

    Add time-based greeting messages in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let hora = 23
+console.log(`são exatamente ${hora} horas.`)
+
+if ( hora >=0 && hora <= 5 ){
+    console.log("é madrugada.")
+    console.log("durma bem.")
+} else if (hora >=6 && hora <= 11){
+    console.log("é manhã.")
+    console.log("bom dia")
+}else if (hora >=12 && hora <=17){
+    console.log("é tarde.")
+    console.log("boa tarde")
+}else{
+    console.log("é noite.")
+    console.log("boa noite, hora de dormir.")
+}
