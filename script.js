db11b8db2decc7bd66ab8dc2e9b9d326960a97fc
Penélope Vieira <vieirapenelope21@gmail.com>
2026-02-19 15:03:06 -0300

    Add time and day of the week logging

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//horas
+
 let agora = new Date()
 let hora = agora.getHours()
 console.log(`são exatamente ${hora} horas.`)
@@ -15,3 +17,42 @@ if ( hora >=0 && hora <= 5 ){
     console.log("é noite.")
     console.log("boa noite, hora de dormir.")
 }
+
+//dia da semana
+let agora = new Date()
+let diaSem = agora.getDay()
+/* 
+    0 = domingo
+    1 = segunda
+    2 = terça
+    3 = quarta
+    4 = quinta
+    5 = sexta
+    6 = sabado
+*/
+
+switch(diaSem){
+    case 0:
+        console.log("domingo")
+        break
+    case 1:
+        console.log("segunda")
+        break
+    case 2:
+        console.log("terça")
+        break
+    case 3:
+        console.log("quarta")
+        break
+    case 4:
+        console.log("quinta")
+        break
+    case 5:
+        console.log("sexta")
+        break
+    case 6:
+        console.log("sabado")
+        break
+    default:
+        console.log("vc ta no nether")
+} 
